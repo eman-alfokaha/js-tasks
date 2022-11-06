7742507759928fc6713fd2d4bdc06c2445dca726
eman-alfokaha <115453404+eman-alfokaha@users.noreply.github.com>
2022-11-06 05:45:21 -0800

    Add files via upload

---
+++ learn-javascript/TASK2/task2.js
@@ -0,0 +1,25 @@
+for (let i=1 ; i<11;i++ ){
+    document.write(i)
+    
+    if (i==2){
+        i+=1
+        document.write(i)
+    }
+
+    if (i==4){
+        i+=1;
+        document.write(i);
+        i+=1;
+        document.write(i);
+    }
+
+    if (i == 7){
+        i+=1;
+        document.write(i);
+        i+=1;
+        document.write(i);
+        i+=1;
+        document.write(i);
+    }
+    document.write("<br/>")
+}
---
+++ learn-javascript/task4/task.js
@@ -0,0 +1,10 @@
+var sum = 0;
+var i = 0;
+while (sum< 100){
+
+
+    document.write(sum)
+    document.write("<br/>")
+    i++;
+    sum +=i;
+}
